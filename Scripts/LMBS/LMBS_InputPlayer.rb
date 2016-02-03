f75df52c616e17686e6fd755477fd2133e694de9
Marc-André Boucher <butchlecolosse@hotmail.com>
2016-02-03 17:32:13 -0500

    Cache command instance

--- Scripts/LMBS/LMBS_InputPlayer.rb
+++ Scripts/LMBS/LMBS_InputPlayer.rb
@@ -7,10 +7,38 @@
 module LMBS
   class LMBS_InputPlayer < LMBS_InputController
     def command
-      return LMBS_GuardCommand.new      if Input.press?(Input::Keys::X)
-      return LMBS_WalkRightCommand.new  if Input.press?(Input::Keys::RIGHT)
-      return LMBS_WalkLeftCommand.new   if Input.press?(Input::Keys::LEFT)
-      return LMBS_IdleCommand.new
+      return guard_command       if Input.press?(Input::Keys::X)
+      return walk_right_command  if Input.press?(Input::Keys::RIGHT)
+      return walk_left_command   if Input.press?(Input::Keys::LEFT)
+      return idle_command
+    end
+    #--------------------------------------------------------------------------
+    # * Idle Command Instance
+    #--------------------------------------------------------------------------
+    def idle_command
+      return @idle_command if @idle_command
+      @idle_command = LMBS_IdleCommand.new
+    end
+    #--------------------------------------------------------------------------
+    # * Walk Right Command Instance
+    #--------------------------------------------------------------------------
+    def walk_right_command
+      return @walk_right_command if @walk_right_command
+      @walk_right_command = LMBS_WalkRightCommand.new
+    end
+    #--------------------------------------------------------------------------
+    # * Walk Left Command Instance
+    #--------------------------------------------------------------------------
+    def walk_left_command
+      return @walk_left_command if @walk_left_command
+      @walk_left_command = LMBS_WalkLeftCommand.new
+    end
+    #--------------------------------------------------------------------------
+    # * Guard Command Instance
+    #--------------------------------------------------------------------------
+    def guard_command
+      return @guard_command if @guard_command
+      @guard_command = LMBS_GuardCommand.new
     end
   end
 end
