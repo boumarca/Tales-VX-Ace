06d05b5d8ea7b10bc7648eba8e0b749cf463b296
Marc-André Boucher <butchlecolosse@hotmail.com>
2016-02-03 17:37:23 -0500

    Adding comments headers to classes

--- Scripts/LMBS/LMBS_Commands.rb
+++ Scripts/LMBS/LMBS_Commands.rb
@@ -5,41 +5,56 @@
 #==============================================================================
 
 module LMBS
+  #==============================================================================
+  # * Command Superclass
+  #============================================================================== 
   class LMBS_Command
     def execute(battler)
     end
   end
-  
+  #==============================================================================
+  # * Idle Command
+  #==============================================================================   
   class LMBS_IdleCommand < LMBS_Command
     def execute(battler)
       battler.idle
     end
   end
-  
+  #==============================================================================
+  # * Walk Right Command
+  #==============================================================================   
   class LMBS_WalkRightCommand < LMBS_Command
     def execute(battler)
       battler.walk_right
     end
   end
-  
+  #==============================================================================
+  # * Walk Left Command
+  #==============================================================================   
   class LMBS_WalkLeftCommand < LMBS_Command
     def execute(battler)
       battler.walk_left
     end
   end
-  
+  #==============================================================================
+  # * Guard Command
+  #==============================================================================   
   class LMBS_GuardCommand < LMBS_Command
     def execute(battler)
       battler.guard
     end
   end
-
+  #==============================================================================
+  # * Run Right Command
+  #============================================================================== 
   class LMBS_RunRightCommand < LMBS_Command
     def execute(battler)
       battler.run_right
     end
   end
-  
+  #==============================================================================
+  # * Run Left Command
+  #==============================================================================   
   class LMBS_RunLeftCommand < LMBS_Command
     def execute(battler)
       battler.run_left
