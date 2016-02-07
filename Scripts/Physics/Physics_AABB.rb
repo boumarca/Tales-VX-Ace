807c33bb0fca703e7b52430091338031c80221a9
Marc-André Boucher <butchlecolosse@hotmail.com>
2016-02-07 16:42:28 -0500

    Complute collisions between two AABB

---
+++ Scripts/Physics/Physics_AABB.rb
@@ -0,0 +1,25 @@
+﻿#==============================================================================
+# ** Physics AABB
+#------------------------------------------------------------------------------
+#  This class handles Axis-Aligned Bounding Box (AABB) and their collisions
+#==============================================================================
+
+class Physics_AABB
+  attr_accessor :min
+  attr_accessor :max
+  #--------------------------------------------------------------------------
+  # * Object Initialization
+  #--------------------------------------------------------------------------
+  def initialize(rect)
+    @min = Vector2.new(rect.x, rect.y)
+    @max = Vector2.new(rect.x + rect.width, rect.y + rect.height)
+  end
+  #--------------------------------------------------------------------------
+  # * Determines if two AABB collides with each other using SAT
+  #--------------------------------------------------------------------------
+  def self.aabb_vs_aabb(a, b)
+    return false if a.max.x < b.min.x || a.min.x > b.max.x
+    return false if a.max.y < b.min.y || a.min.y > b.max.y
+    return true
+  end
+end
